be9c434b316186bf100ba34abc69a3148892d933
Philip McGarvey <2015mbp8gb128gb@2015s-MacBook-Pro.local>
2025-02-25 20:03:32 -0500

    changes

--- main.sh
+++ main.sh
@@ -6,5 +6,21 @@ function sm() {
 }
 
 function sort-modify() {
-  bash "$ASCR_DIR/sort-modify-dates.sh"
+  # updates all files last modified dates in current directory so that a last-modified sort gives same results as alphabetical sort
+
+# Get the most recent modification time in the directory (in seconds since epoch)
+latest_file=$(ls -t | head -n 1)
+latest_timestamp=$(stat -f "%m" "$latest_file")
+
+# Sort files alphabetically and update timestamps
+counter=0
+for file in $(ls -1 | sort); do
+    if [[ -f "$file" ]]; then
+        new_time=$((latest_timestamp + counter))
+        echo "$new_time"
+        touch -t "$(date -u -r "$new_time" +%Y%m%d%H%M.%S)" "$file"
+        ((counter++))
+    fi
+done
+
 }
